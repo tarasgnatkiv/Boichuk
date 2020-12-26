ded3dc1a5c09222805325e44a17f21d1b0e99293
tarasgnatkiv <tarasgnatkiv@gmail.com>
2020-12-26 17:48:17 +0200

    users

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -3,67 +3,274 @@ import "../../../node_modules/@fortawesome/fontawesome-free/css/all.css";
 import classes from "./User.module.css";
 import femaleUser from "./femaleUser.png";
 import ButtonComponent from "../UI/Button/Button";
-class User extends Component {
-  state = {};
-  render() {
-    return (
-      <li className={classes.User}>
-        <div className={classes.UserMainPart}>
-          <div className={classes.LeftPart}>
-            <div className={classes.DeleteUser}>
-              <i class="fas fa-user-minus"></i>
-            </div>
-            <img className={classes.Avatar} src={femaleUser} />
-          </div>
-          <div className={classes.RightPart}>
-            <div className={classes.TopUserHeader}>
-              <div className={classes.Label}>{this.props.userName}</div>
-            </div>
-            <div className={classes.TaskGroup}>
-              <style>
-                @import
-                url('https://fonts.googleapis.com/css2?family=Langar&family=Pacifico&family=Questrial&display=swap');
-              </style>
-              <div>Tasks:</div>
-              <div className={classes.TaskGroupValue}>
-                <i class="fas fa-tasks"></i>
-              </div>
-              <div className={classes.TaskGroupValue}>
-                {this.props.userTasks}
-              </div>
-            </div>
-            <div className={classes.RatingGroup}>
-              <style>
-                @import
-                url('https://fonts.googleapis.com/css2?family=Langar&family=Pacifico&family=Questrial&display=swap');
-              </style>
-              <div>Rating:</div>
-              <div className={classes.RatingGroupValue}>
-                <i class="far fa-smile"></i>
-              </div>
-              <div className={classes.TaskGroupValue}>{"88/100"}</div>
-            </div>
-            <button className={classes.ButtonAddTask}>Add task</button>
-          </div>
-        </div>
-        <div className={classes.BottomRemove}>
-          <div className={classes.container}>
-            <h1>Delete User</h1>
-            <p>Are you sure you want to delete your worker?</p>
-            <div className={classes.clearfix}>
-              <button type="button" className={classes.cancelbtn}>
-                Cancel
-              </button>
-              <button type="button" className={classes.deletebtn}>
-                Delete
-              </button>
-            </div>
-          </div>
-        </div>
+// import InputElement from "../../../components/UI/Input/Input";
+// import ButtonElement from "../../../components/UI/Button/Button";
 
-  
-      </li>
-    );
-  }
+import InputElement from "../../components/UI/Input/Input";
+import ButtonElement from "../../components/UI/Button/Button";
+import { Redirect } from "react-router-dom";
+import Spinner from "../../components/UI/Spinner/Spinner";
+
+
+
+class User extends Component {
+    state = {
+        delete: false,
+        addTask: false,
+        controls: {
+            WorkName: {
+                elementType: "input",
+                elementConfig: {
+                    type: "",
+                    placeholder: "Dota 2",
+                },
+                label: "Work name",
+                touched: false,
+                value: "",
+                validation: {
+                    required: true,
+                    valid: false,
+                    minLength: 1,
+                    maxLength: 20,
+                    isEmail: false,
+                },
+            },
+            WorkDescription: {
+                elementType: "textarea",
+                elementConfig: {
+                    type: "",
+                    placeholder:
+                        "At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies.",
+                },
+                label: "Description",
+                touched: false,
+                value: "",
+                validation: {
+                    required: true,
+                    valid: false,
+                    minLength: 3,
+                    maxLength: 500,
+                },
+            },
+        },
+        signUp: true,
+    };
+
+    // componentDidMount() {
+    //     this.props.setRedirectPath("null");
+    // }
+    checkValidity(value, rules) {
+        let isValid = true;
+        if (!rules) {
+            return true;
+        }
+        if (rules.required) {
+            isValid = value.trim() !== "" && isValid;
+        }
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+        }
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid;
+        }
+        if (rules.isEmail) {
+            const pattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+            console.log(value);
+            isValid = pattern.test(value) && isValid;
+            console.log(isValid);
+        }
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid;
+        }
+        return isValid;
+    }
+    inputChangedHandler = (event, controlName) => {
+        const updatedControls = {
+            ...this.state.controls,
+            [controlName]: {
+                ...this.state.controls[controlName],
+                value: event.target.value,
+                validation: {
+                    ...this.state.controls[controlName].validation,
+                    valid: this.checkValidity(
+                        event.target.value,
+                        this.state.controls[controlName].validation
+                    ),
+                },
+                touched: true,
+            },
+        };
+        this.setState({ controls: updatedControls });
+    };
+    submitHandler = (event) => {
+        event.preventDefault();
+        this.props.onCreateWork(
+            this.state.controls.WorkName.value,
+            this.state.controls.WorkDescription.value,
+            this.props.userId,
+            this.props.token
+        );
+    };
+
+    cancelDelete = () => {
+        if (this.state.addTask) {
+            this.setState({
+                addTask: !this.state.addTask,
+            });
+        }
+        this.setState({
+            delete: !this.state.delete,
+        });
+    };
+
+    addTaskFunc = () => {
+        if (this.state.delete) {
+            this.setState({
+                delete: !this.state.delete,
+            });
+        }
+        this.setState({
+            addTask: !this.state.addTask,
+        });
+    };
+
+    render() {
+        const formElementsArray = [];
+        for (let key in this.state.controls) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.controls[key],
+            });
+        }
+        let form = formElementsArray.map((element) => {
+            return (
+                <InputElement
+                    key={element.id}
+                    changed={(event) => this.inputChangedHandler(event, element.id)}
+                    elementType={element.config.elementType}
+                    elementConfig={element.config.elementConfig}
+                    invalid={!element.config.validation.valid}
+                    value={element.config.value}
+                    shouldValidate={element.config.validation.required}
+                    touched={element.config.touched}
+                    label={element.config.label}
+                />
+            );
+        });
+        let error = null;
+        if (this.props.error) {
+            error = (
+                <React.Fragment>
+                    <style>
+                        @import
+                        url('https://fonts.googleapis.com/css2?family=Russo+One&display=swap');
+          </style>
+                    <div className={classes.errorMessage}>{this.props.error}</div>
+                </React.Fragment>
+            );
+            console.log("fefef");
+        }
+        let spinner = null;
+        if (this.props.loading) {
+            spinner = <Spinner />;
+        }
+        let redirect = null;
+        if (this.props.redirect) {
+            redirect = <Redirect to={this.props.redirect} />;
+        }
+        if (this.props.redirect == null || this.props.redirect == "null") {
+            redirect = null;
+        }
+
+        return (
+            <li className={classes.User}>
+                <div className={classes.UserMainPart}>
+                    <div className={classes.LeftPart}>
+                        <div className={classes.DeleteUser} onClick={this.cancelDelete}>
+                            <i class="fas fa-user-minus"></i>
+                        </div>
+                        <img className={classes.Avatar} src={femaleUser} />
+                    </div>
+                    <div className={classes.RightPart}>
+                        <div className={classes.TopUserHeader}>
+                            <div className={classes.Label}>{this.props.userName}</div>
+                        </div>
+                        <div className={classes.TaskGroup}>
+                            <style>
+                                @import
+                                url('https://fonts.googleapis.com/css2?family=Langar&family=Pacifico&family=Questrial&display=swap');
+                      </style>
+                            <div>Tasks:</div>
+                            <div className={classes.TaskGroupValue}>
+                                <i class="fas fa-tasks"></i>
+                            </div>
+                            <div className={classes.TaskGroupValue}>
+                                {this.props.userTasks}
+                            </div>
+                        </div>
+                        <div className={classes.RatingGroup}>
+                            <style>
+                                @import
+                                url('https://fonts.googleapis.com/css2?family=Langar&family=Pacifico&family=Questrial&display=swap');
+                      </style>
+                            <div>Rating:</div>
+                            <div className={classes.RatingGroupValue}>
+                                <i class="far fa-smile"></i>
+                            </div>
+                            <div className={classes.TaskGroupValue}>{"88/100"}</div>
+                        </div>
+                        <button className={classes.ButtonAddTask} onClick={this.addTaskFunc}>Add task</button>
+                    </div>
+                </div>
+                <div
+                    className={
+                        this.state.delete
+                            ? [classes.active, classes.DeleteContainer].join(" ")
+                            : classes.DeleteContainer
+                    }
+                >
+                    <div className={classes.container}>
+                        <h1>Delete User</h1>
+                        <p>Are you sure you want to delete your user?</p>
+
+                        <div className={classes.clearfix}>
+                            <button
+                                type="button"
+                                onClick={this.cancelDelete}
+                                className={classes.cancelbtn}>Cancel
+                            </button>
+                            <button
+                                type="button"
+                                className={classes.deletebtn}>Delete
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={this.state.addTask ? [classes.active, classes.addTaskContainer].join(" ") : classes.addTaskContainer}>
+                    <div className={classes.addTask}>
+                        <h1>Add task</h1>
+                        <form className={classes.form} onSubmit={this.submitHandler}>
+                            {form}
+                            {error}
+                            {/* <ButtonElement btnType="Success">CREATE</ButtonElement> */}
+                            <div className={classes.clearfix}>
+                                <button
+                                    type="button"
+                                    onClick={this.addTaskFunc}
+                                    className={classes.cancelbtn}>Cancel
+                                </button>
+                                <button
+                                    type="button"
+                                    className={classes.deletebtn}>Create
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </li>
+        );
+    }
 }
 export default User;
